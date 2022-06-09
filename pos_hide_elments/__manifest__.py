14669a8d28d609e07e4c9cd0d2b7bbc8031b282a
sinergia-e <73215000+sinergia-e@users.noreply.github.com>
2022-06-09 13:41:37 -0500

    Add files via upload

---
+++ pos_hide_elments/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2019 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Ocultar Elementos Pos",
+    "summary": "Este campo permite quitar elementos en el pos",
+    "version": "15.0.1.0.0",
+    "author": "Jesus pozzo",
+    "website": "",
+    "license": "AGPL-3",
+    "category": "Point Of Sale",
+    "depends": ["point_of_sale"],
+    "data": [
+
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+        ],
+        'web.assets_qweb': ['pos_hide_elments/static/src/xml/ProductInfoPopup.xml'],
+    },
+
+    "installable": True,
+}
