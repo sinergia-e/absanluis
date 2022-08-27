dfdd50f26fc72a9f1ad606df26988d040622553a
sinergia-e <73215000+sinergia-e@users.noreply.github.com>
2022-08-27 01:44:45 -0500

    Add files via upload

---
+++ pos_price_checker/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Verificadores San Luis',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'sequence': 6,
+    'author': 'Sinergia',
+    'summary': 'Usa terminal como Verificador.',
+    'description': """
+
+=======================
+Permite usar POS como verificador.
+
+""",
+    'depends': ['point_of_sale'],
+    'data': [
+            'views/views.xml'
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_price_checker/static/src/js/jquery-ui.css',
+            'pos_price_checker/static/src/js/pos.js',
+            'pos_price_checker/static/src/css/pos.css',
+        ],
+        'web.assets_qweb': [
+            'pos_price_checker/static/src/xml/**/*',
+        ],
+    },
+    'images': [
+        'static/description/sele.jpg',
+    ],
+    'installable': True,
+    'website': '',
+    'auto_install': False,
+    'price': 100,
+    'currency': 'EUR',
+}
