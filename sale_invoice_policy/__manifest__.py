1d9ab56a8be3fd319c287c6b2017db4ced9a32a6
sinergia-e <73215000+sinergia-e@users.noreply.github.com>
2022-10-04 00:00:02 -0500

    Update __manifest__.py

--- sale_invoice_policy/__manifest__.py
+++ sale_invoice_policy/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Politica de Facturacion San Luis",
     "summary": """
